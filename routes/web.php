4a10b21da3b1ef5fcd210f88888763898b0e31c3
Pedro Moura <pedro.moura@imusica.com.br>
2019-10-08 11:43:20 -0300

    Habilitando no routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+Auth::routes();
+
 Route::middleware(['auth'])->group(function (){
     Route::get('/escolas/novo','EscolaController@novo')->name('novo_cadastro_escola');
     Route::get('/escolas/listar','EscolaController@listar')->name('listar_escola');
